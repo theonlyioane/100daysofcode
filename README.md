3ae4bfb24186ed805fe0feea2f2094011b21da61
theonlyioane <theonlyioane@gmail.com>
2016-06-12 10:41:45 -0400

    added tutorials to day2

--- README.md
+++ README.md
@@ -8,4 +8,8 @@ I've also added in notes to my JS so that I'm understanding the concepts that I'
 ###Day 2
 For this excercise I wanted to mock up the action of a user clicking a button and being able to reference different content withing the same module. I was able to do that by setting functions with different strings, and tieing them within the same elementId
 
+Started working through the examples in 
+https://www.amazon.com/JavaScript-JQuery-Interactive-Front-End-Development/dp/1118531647?ie=UTF8&ref_=asap_bc
+
+Went through the basics, as well so the date and price tutorials. Currently on page 64.
 
--- day2/day2.js
+++ day2/day2.js
@@ -1,14 +1,47 @@
-//I've set my function as content 1
-function content1 () {
+//
+//Changes the demo tag content based on the function that is called
+//
+function content1 () {//I've set my function as content 1
 	//I want the function to find the id="demo" tag in the html
 	//and place the following string in that tag
 	document.getElementById("demo").innerHTML = "Content 1 is visible.";
 }
-
 function content2 () {
 	document.getElementById("demo").innerHTML = "Content 2 is visible."
 }
-
 function content3 () {
 	document.getElementById("demo").innerHTML = "Content 3 is visible."
 }
+
+
+//
+//Creates a greeting based on the users current time
+//
+var today = new Date (); // Create a new date object
+var hourNow = today.getHours (); // Finds the current hour
+var greeting;
+// Displays the appropriate greeting based on the current time
+if (hourNow > 18) {
+	greeting = 'Good evening!';
+}	else if (hourNow > 12) {
+	greeting = 'good afternoon!'
+}	else if (hourNow > 0) {
+	greeting ='Good Morning!'
+}	else {
+	greeting ='Welcome!'
+}
+document.write ('<h3>' + greeting +'</h3>');
+
+//
+//Adding multiple variables together
+//
+var price; //declared the var price
+var quantity; //declared the variable quantity
+var price; //declared the varaible price
+
+price=5; //assigns the value of 5 to the variable price
+quantity=14; //assigns the value of 15 to the variable of quantity
+total= price * quantity; //assigns the value of the variables total and price multiplied together
+
+var el = document.getElementById('cost'); //ties the function to the id 'cost' in the html file
+el.textContent = '$' + total; //adds a '$' sysmbol to the front of the total value
